ee4d28ee4d33b054bd730e7c90c3444401838185
victorl1311 <victorlau1311@hotmail.com>
2025-09-07 12:20:49 -0500

    R

--- Python/Script/code.py
+++ Python/Script/code.py
@@ -1,8 +1,10 @@
 import numpy as np
-import pandas as pd
 import seaborn as sns
+import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 import os
+
 
 os.chdir(r"C:\Users\VICTOR\Documents\GitHub\High_Dimensional_Linear_Models")
 
@@ -13,8 +15,8 @@ df.head()
 df["area2"] = df["area"]**2
 
 #a2
-cols_dummy = ["hasparkingspace", "hasbalcony", "haselevator", "hassecurity", "hasstorageroom"]
-for col in cols_dummy:
+h_dummies = ["hasparkingspace", "hasbalcony", "haselevator", "hassecurity", "hasstorageroom"]
+for col in h_dummies:
     df[col] = df[col].map({"yes": 1, "no": 0})
 
 #a3
@@ -23,5 +25,12 @@ for i in range(10):
     df[f"end_{i}"] = (df["area_numero"] == i).astype(int)
 
 #b1
+categorical_cols = ['month', 'type', 'rooms', 'ownership', 'buildingmaterial']
+df = pd.get_dummies(df, columns=categorical_cols, drop_first=True)
+end_dummies = [f'end_{i}' for i in range(9)]
+x_variables = ['area', 'area2', 'distance_to_school', 'distance_to_clinic', 'distance_to_postoffice']+h_dummies+end_dummies
+
+
+
 
 
